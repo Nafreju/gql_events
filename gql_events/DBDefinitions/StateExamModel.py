b231e704a3182164c9450dfe1ad6018c5b9b6483
Niko1s <nikola.sedlackova@centrum.cz>
2023-11-23 15:19:38 +0100

    StateExamModel created

---
+++ gql_events/DBDefinitions/StateExamModel.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, DateTime, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.functions import now
+
+from .uuid import UUIDColumn, UUIDFKey
+from .base import BaseModel
+
+
+class StateExamModel(BaseModel):
+    pass
